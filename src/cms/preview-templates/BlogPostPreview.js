88cc94a61911b79c56ac9104f225d872a9e26859
Maciej Leszczyński <dev@asista.pl>
2019-01-30 14:02:53 +0100

    Update BlogPostPreview.js

--- src/cms/preview-templates/BlogPostPreview.js
+++ src/cms/preview-templates/BlogPostPreview.js
@@ -3,17 +3,25 @@ import PropTypes from 'prop-types'
 
 import { BlogPostTemplate } from '../../templates/blog-post'
 
-const BlogPostPreview = ({ entry, widgetFor }) => (
-  <Fragment>
-    <BlogPostTemplate
-      title={entry.getIn(['data', 'title'])}
-      date={widgetFor('date')}
-      description={entry.getIn(['data', 'description'])}
-      content={widgetFor('body')}
-      tags={Array.from(entry.getIn(['data', 'tags']))}
-    />
-  </Fragment>
-)
+const BlogPostPreview = ({ entry, widgetFor }) => {
+  let tags = entry.getIn(['data', 'tags'])
+
+  // Make sure tags filed exist and make sure it's an array
+  // because Preview component requires an array
+  tags = tags ? Array.from(tags) : tags
+
+  return (
+    <Fragment>
+      <BlogPostTemplate
+        title={entry.getIn(['data', 'title'])}
+        date={widgetFor('date')}
+        description={entry.getIn(['data', 'description'])}
+        content={widgetFor('body')}
+        tags={tags}
+      />
+    </Fragment>
+  )
+}
 
 BlogPostPreview.propTypes = {
   entry: PropTypes.shape({
